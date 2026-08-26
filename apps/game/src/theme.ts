5892bb70200d40d7fd63b2751b90972a069aa3e4
Thomas Klein <tmkn@users.noreply.github.com>
2026-08-26 19:53:35 +0200

    chore: wip hallways (#11)

---
+++ apps/game/src/theme.ts
@@ -0,0 +1,31 @@
+const CatppuccinMacchiato = {
+    rosewater: 0xf4dbd6,
+    flamingo: 0xf0c6c6,
+    pink: 0xf5bde6,
+    mauve: 0xc6a0f6,
+    red: 0xed8796,
+    maroon: 0xee99a0,
+    peach: 0xf5a97f,
+    yellow: 0xeed49f,
+    green: 0xa6da95,
+    teal: 0x8bd5ca,
+    sky: 0x91d7e3,
+    sapphire: 0x7dc4e4,
+    blue: 0x8aadf4,
+    lavender: 0xb7bdf8,
+    text: 0xcad3f5, // Main text
+    subtext1: 0xb8c0e0, // Secondary text
+    subtext0: 0xa5adce, // Tertiary text
+    overlay2: 0x939ab7, // Subtle borders/highlights
+    overlay1: 0x8087a2, // Brighter borders/highlights
+    overlay0: 0x6e738d, // Even brighter borders/UI elements
+    surface2: 0x5b6078, // Lighter surface
+    surface1: 0x494d64, // Mid surface
+    surface0: 0x363a4f, // Darker surface
+    base: 0x24273a, // Main background
+    mantle: 0x1e2030, // Secondary background (slightly darker)
+    crust: 0x181926 // Tertiary background (darkest)
+};
+
+// Choose the theme
+export const Theme = CatppuccinMacchiato;
